b8c5f67fdf628aae344cb061635acb0c8f67368a
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-22 18:01:23 +0100

    create dto for create request

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,3 +1,4 @@
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
@@ -10,11 +11,10 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
     const task =  {
+      ...createTaskDto,
       id: uuid(),
-      title,
-      description,
       status: TaskStatus.OPEN
     }
 
