f0e8fae0184a00368e5480545f0d1694b0ae02fc
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-23 06:15:43 +0100

    add tasks repository

---
+++ src/tasks/tasks.repository.ts
@@ -0,0 +1,14 @@
+import { DataSource, Repository } from 'typeorm';
+import { Task } from './tasks.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TaskRepository extends Repository<Task> {
+  constructor(private dataSource: DataSource) {
+    super(Task, dataSource.createEntityManager());
+  }
+
+  async getById(id: string) {
+    return this.findOne({ where: { id } });
+  }
+}
