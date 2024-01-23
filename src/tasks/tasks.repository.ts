2d93bfca9adb9535020a2b41c382152e85697b80
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-23 08:46:17 +0100

    repository - add method for addint task

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,10 +1,21 @@
-import { DataSource, EntityRepository, Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Task } from './tasks.entity';
 import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task.enum';
 
 @Injectable()
 export class TasksRepository extends Repository<Task> {
   constructor(private dataSource: DataSource) {
     super(Task, dataSource.createEntityManager());
+  }
+
+  async add(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.create({
+      ...createTaskDto,
+      status: TaskStatus.OPEN,
+    });
+    await this.save(task);
+    return task;
   }
 }
