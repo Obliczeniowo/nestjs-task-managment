9a4d49937027cf859018dd56c4ec393b3ea79f64
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-23 14:07:38 +0100

    encrypt password with solt

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -2,6 +2,7 @@ import { ConflictException, Injectable, InternalServerErrorException } from '@ne
 import { User } from './user.entity';
 import { DataSource, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
@@ -10,7 +11,14 @@ export class UsersRepository extends Repository<User> {
   }
 
   async add(credentials: AuthCredentialsDto): Promise<void> {
-    const user = this.create(credentials);
+    const salt = await bcrypt.genSalt();
+    const passwordHashed = await bcrypt.hash(credentials.password, salt);
+
+    const user = this.create({
+      ...credentials,
+      password: passwordHashed
+    });
+
     try {
       await this.save(user);
     } catch (error) {
