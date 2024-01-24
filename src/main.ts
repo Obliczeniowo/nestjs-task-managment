93a250ab0bb123cde0f775dbc67af4e992b2ba74
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-24 12:36:50 +0100

    create logger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
+
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor())
 
-  await app.listen(10000);
+  const port = 10000;
+  await app.listen(port);
+  logger.log(`Listening on port ${port}`);
 }
 bootstrap();
