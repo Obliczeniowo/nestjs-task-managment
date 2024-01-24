0cfa01989dac965b4103cb003d5e10f31714fbb6
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-24 14:36:11 +0100

    make dependency of config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,34 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+console.log(`.env.stage.${process.env.npm_lifecycle_event.split(':')[1]}`);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.stage.${process.env.npm_lifecycle_event.split(':')[1]}`
+    }),
     TasksModule,
     AuthModule,
-    TypeOrmModule.forRoot(
-      {
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'task-management',
-        autoLoadEntities: true,
-        synchronize: true
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: config.get('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get('DB_USERNAME'),
+          password: config.get('DB_PASSWORD'),
+          database: config.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true
+        }
       }
-    ),
+    }),
   ],
 })
-export class AppModule {}
+export class AppModule { }
