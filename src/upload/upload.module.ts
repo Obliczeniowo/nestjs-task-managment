dfa2afc6e8b920e5f4d149f090be0d4e213a68c5
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-25 04:28:16 +0100

    configure multer save file

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { UploadController } from './upload/upload.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { diskStorage } from 'multer';
 
 @Module({
-  controllers: [UploadController]
+  controllers: [UploadController],
+  imports: [MulterModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => (
+      {
+        storage: diskStorage({
+          destination: './uploaded-files',//configService.get<string>('MULTER_DEST'), // Tutaj podaj ścieżkę do folderu, gdzie chcesz zapisywać pliki
+          filename: (req, file, callback) => {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+            callback(null, file.fieldname + '-' + uniqueSuffix + file.originalname);
+          },
+        }),
+      }
+    ),
+    inject: [ConfigService],
+  }),]
 })
-export class UploadModule {}
+export class UploadModule { }
