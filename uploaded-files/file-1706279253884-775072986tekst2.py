5f94413aa1cb34c1e3150421b76a66ea7ffd17f0
Obliczeniowo <zajaczkowskikrzysztofkz@gmail.com>
2024-01-26 17:07:57 +0100

    example of files

---
+++ uploaded-files/file-1706279253884-775072986tekst2.py
@@ -0,0 +1,6 @@
+﻿name = input("Podaj imię: ")
+surname = input("Podaj nazwisko: ")
+age = int(input("Podaj wiek: "))
+weight = float(input("Podaj wagę [kg]: "))
+
+print("Imię: %s\nNazwisko: %s\n%s\nWiek: %3d\nWaga [kg]: %5.1f"%(name, surname, "=" * 70, age, weight))
